67a3b98664d68edc701750b31a4f9f01e0629267
FlodCoding <openthedoorcia@163.com>
2020-05-07 10:10:23 +0800

    Initial Commit

---
+++ TodoList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TodoList
+//
+//  Created by Flood on 2020/5/7.
+//  Copyright © 2020 Flood. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
